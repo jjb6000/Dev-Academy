d8b4d5ee9b6ee1cb795f2500329fa64b169787e8
jjb6000 <jonas.benkwitz@gmail.com>
2024-01-17 16:08:10 +0100

    calc ready

--- Kochwelt/app.js
+++ Kochwelt/app.js
@@ -1,12 +1,13 @@
+let previousMultiplicator = 1
 
-
-function calculateAmount () {
+function calculateAmount() {
     const elements = document.getElementsByClassName('variable');
     let multiplicator = document.getElementById('input-text').value
     
     for (let i = 0; i < elements.length; i++) {
-        let newAmount = elements[i].innerHTML * multiplicator;        
+        let newAmount = (elements[i].innerHTML / previousMultiplicator) * multiplicator;        
         elements[i].innerHTML = `${newAmount}`
     }
+    previousMultiplicator = multiplicator
 
 }
