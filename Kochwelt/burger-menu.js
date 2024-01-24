5367398961a29fa3b83d996501874c73ad82881b
jjb6000 <jonas.benkwitz@gmail.com>
2024-01-24 12:32:31 +0100

    kochwelt burger menu done

---
+++ Kochwelt/burger-menu.js
@@ -0,0 +1,21 @@
+
+
+function openMenu() {
+    mobileMenu = document.getElementById('navbar-mobile');
+    mobileMenu.style.display = "flex";
+
+    mobileMenu.innerHTML = /*html*/`
+                <img onclick="closeMenu(mobileMenu)" class="closeBurger" src="img/icons/close.svg" alt="close menu">
+                <a href="index.html" class="navlink-mobile">Start</a>
+                <a href="rezept-moussaka.html" class="navlink-mobile">Rezept des Tages</a>
+                <a href="#" class="navlink-mobile">Kontakt</a>
+                <a href="#" class="navlink-mobile">Impressum</a>
+    `
+}
+
+function closeMenu(mobileMenu) {
+    mobileMenu.innerHTML = /*html*/`  `;
+    mobileMenu.style.display = "none"
+}
+
+
