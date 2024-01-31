d5eaaa3ec16bccc9f9230786f7f91e07e53f5866
jjb6000 <jonas.benkwitz@gmail.com>
2024-01-31 17:06:40 +0100

    transition

--- Kochwelt/burger-menu.js
+++ Kochwelt/burger-menu.js
@@ -1,21 +1,12 @@
 
 
-function openMenu() {
-    mobileMenu = document.getElementById('navbar-mobile');
-    mobileMenu.style.display = "flex";
+function openMenu() {;
+    document.getElementById('navbar-mobile').classList.add('show-mobile-nav');
 
-    mobileMenu.innerHTML = /*html*/`
-                <img onclick="closeMenu(mobileMenu)" class="closeBurger" src="img/icons/close.svg" alt="close menu">
-                <a href="index.html" class="navlink-mobile">Start</a>
-                <a href="rezept-moussaka.html" class="navlink-mobile">Rezept des Tages</a>
-                <a href="contact.html" class="navlink-mobile">Kontakt</a>
-                <a href="impressum.html" class="navlink-mobile">Impressum</a>
-    `
 }
 
-function closeMenu(mobileMenu) {
-    mobileMenu.innerHTML = /*html*/`  `;
-    mobileMenu.style.display = "none"
+function closeMenu() {
+    document.getElementById('navbar-mobile').classList.remove('show-mobile-nav');
 }
 
 
