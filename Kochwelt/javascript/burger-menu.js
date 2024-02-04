d1609703e42de03795c07348780db6b05d570f60
jjb6000 <jonas.benkwitz@gmail.com>
2024-02-04 13:31:10 +0100

    bugfixes header Kochwelt + js folder

--- Kochwelt/javascript/burger-menu.js
+++ Kochwelt/javascript/burger-menu.js
@@ -1,12 +1,21 @@
+let open = false 
 
+function isItOpen () {
+    if (open === false) {
+        openMenu();
+    } else {
+        closeMenu();
+    }
+} 
 
 function openMenu() {;
     document.getElementById('navbar-mobile').classList.add('show-mobile-nav');
-
+    open = true
 }
 
 function closeMenu() {
     document.getElementById('navbar-mobile').classList.remove('show-mobile-nav');
+    open = false
 }
 
 
