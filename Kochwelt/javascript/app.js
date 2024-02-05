0efb0c9d7311d6c4cd75157fa3f01f197f6e9922
jjb6000 <jonas.benkwitz@gmail.com>
2024-02-05 17:16:55 +0100

    kochwelt, fonts, footer, bug fixes

--- Kochwelt/javascript/app.js
+++ Kochwelt/javascript/app.js
@@ -1,24 +1,38 @@
 const standardValue = document.getElementById('input-text').value
 let amountSpans = document.getElementsByClassName('variable');
 let basicArray = []
+let basicArrayXstandardvalue = []
 
 function getBasicAmounts() {
     for (let i = 0; i < amountSpans.length; i++) {
         let base = amountSpans[i].innerHTML / standardValue
-        basicArray.push(base.toFixed(2));   
+        basicArray.push(base.toFixed(2));
     }
+    for (let i = 0; i < amountSpans.length; i++) {
+        let base = amountSpans[i].innerHTML
+        basicArrayXstandardvalue.push(base);
+    }
+
+    console.log(basicArray);
+    console.log(basicArrayXstandardvalue);
 }
 
 function calculateAmount() {
-    
+
     let multiplicator = document.getElementById('input-text').value
 
-    if (multiplicator > 0 ) {
+    if (multiplicator === standardValue) {
+        for (let i = 0; i < basicArrayXstandardvalue.length; i++) {
+            let newAmount = basicArrayXstandardvalue[i];
+            amountSpans[i].innerHTML = `${newAmount}`;
+        }
+
+    } else if (multiplicator > 0) {
         for (let i = 0; i < basicArray.length; i++) {
-            let newAmount = basicArray[i] * multiplicator;        
+            let newAmount = basicArray[i] * multiplicator;
             amountSpans[i].innerHTML = `${newAmount}`;
         }
     } else {
         alert("Nur positive Zahlen");
-    }    
+    }
 }
