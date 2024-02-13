4ce728daefa7cad6138e8232fd7b36569cd7f1b6
jjb6000 <jonas.benkwitz@gmail.com>
2024-02-13 09:51:16 +0100

    KOCHWELT FEEDBACK FORM

--- Kochwelt/javascript/form.js
+++ Kochwelt/javascript/form.js
@@ -1,3 +1,6 @@
+
+let form = document.getElementById('contactForm');
+
 function sendMail(event){﻿
     event.preventDefault();
     const data = new FormData(event.target);
@@ -9,11 +12,32 @@ function sendMail(event){﻿
             'Accept': 'application/json'
         }
     }).then(() => {
-        // window.location.href = "./send_mail.html";
-        document.getElementById('contactForm').innerHTML = /*html*/`
+        // form = email unterwegs
+        form.innerHTML = /*html*/`
             <h2>Email ist Unterwegs</h2>
-        `
+        `;
+        // 5 sekunden Verzögerung bis reloadForm funktion startet
+        setTimeout(reloadForm, 5000);
+
     }).catch((error) => {
         console.log(error);
     });
+}
+
+function reloadForm () {
+    form.innerHTML = /*html*/`
+                <h2>Kontaktformular</h2>
+            <div class="hero-content">
+                <label>Deine email-Adresse:</label>
+                <input class="contact-input" type="email" name="email" required>
+            </div>
+
+            <div class="hero-content">
+                <label>Deine Nachricht:</label>
+                <textarea class="contact-input height-300" name="message" required></textarea>
+            </div>
+
+            <button class="btn" type="submit">Senden</button>
+    `
+    
 }
