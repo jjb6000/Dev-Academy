fb44fa2a53d39d2c7eb83f82cefdbf9459d3291e
jjb6000 <jonas.benkwitz@gmail.com>
2024-04-13 00:18:34 +0200

    tic tac to 1.0

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -7,13 +7,60 @@ let winningCombinations = [
     ["field2", "field5", "field8"],
     ["field0", "field4", "field8"],
     ["field2", "field4", "field6"],
-  ];
-  
+];
+
 
 let circle = document.getElementById('circle');
 let cross = document.getElementById('cross');
 let turn = circle;
 let setInArray = 'circle';
+
+
+function game(e) {
+    document.getElementById(e.target.id).append(turn.cloneNode(true));
+    document.getElementById(e.target.id).removeAttribute('onclick');
+    setPlayerToWinningConditionsArray(e.target.id);
+    checkWin();
+    next();
+}
+
+
+function setPlayerToWinningConditionsArray(field) {
+    for (let i = 0; i < winningCombinations.length; i++) {
+        const combination = winningCombinations[i];
+        if (combination.indexOf(field) > -1) {
+            const index = combination.indexOf(field);
+            combination[index] = setInArray;
+        }
+    }
+}
+
+
+
+function checkWin() {
+    for (let i = 0; i < winningCombinations.length; i++) {
+        if (circleWins(winningCombinations[i])) {
+            showWinner('Kreis')
+            break
+        };
+
+        if (crossWins(winningCombinations[i])) {
+            showWinner('Kreuz')
+            break
+        }
+    }
+}
+
+
+function circleWins(array) {
+    return JSON.stringify(array) == JSON.stringify(['circle', 'circle', 'circle']);
+}
+
+
+function crossWins(array) {
+    return JSON.stringify(array) == JSON.stringify(['cross', 'cross', 'cross']);
+}
+
 
 function next() {
     if (turn == circle) {
@@ -26,38 +73,8 @@ function next() {
 }
 
 
-
-function game(e) {
-    document.getElementById(e.target.id).append(turn.cloneNode(true));
-    document.getElementById(e.target.id).removeAttribute('onclick')
-    next(); 
-}
-
-
-function checkWin() {
-    for (let i = 0; i < winningCombinations.length; i++) {
-        if (circleWins(winningCombinations[i])) {
-            showWinner('Kreis')
-        }
-        if (crossWins(winningCombinations[i])) {
-            showWinner('Kreuz')
-        }        
-    }
-}
-
-
-function circleWins(array) {
-    return array == ['circle', 'circle', 'circle']
-}
-
-
-function crossWins(array) {
-    return array == ['cross', 'cross', 'cross']
-}
-
 function showWinner(player) {
     document.getElementById('win-container').innerHTML = /*html*/`
         ${player} hat gewonnen!!
     `;
-    return
 }
