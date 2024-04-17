6ffb9b2b9572586427000365e422a57fb33d6be5
jjb6000 <jonas.benkwitz@gmail.com>
2024-04-17 16:52:38 +0200

    response check w/  .ok

--- API-chart/include.js
+++ API-chart/include.js
@@ -8,12 +8,15 @@ async function includeHTML() {
     // liest Wert des Attributs aus = header.html
     file = element.getAttribute("w3-include-html") // header.html ist der Dateipfad zur header Datei
     // und speichert Dateipfad in variable 'file'
-    
+
     // jetzt fetchen wir die Datei und speichern in der variable reponse
     let resp = await fetch(file);
-    let content = await resp.text(); // resp als text in variable content speichern
-    
-    element.innerHTML = content; // contert in den container einfügen
+    if (resp.ok) {
+      element.innerHTML = await resp.text(); // resp als text in variable in div einfügen
+    } else {
+      element.innerHTML = 'page not found'
+    }
+        
   }
 }
 
