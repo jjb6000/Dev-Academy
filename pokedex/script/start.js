9d821dfec2634755f264f16e93f6842938a6c501
jjb6000 <jonas.benkwitz@gmail.com>
2024-05-02 11:24:13 +0200

    ideas for start js

--- pokedex/script/start.js
+++ pokedex/script/start.js
@@ -11,5 +11,17 @@ function getNames(params) {
 
 
 function getCardDetails() {
+
+}
+
+
+function defineColor(params) {
+    // group pokemons in arrays to define which groups need same color
+}
+
+
+function colorOrder() {
     
+    // if last color green
+    //  then red , if red then blue, if blue then yellow
 }
