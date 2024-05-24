2f59c866540c5f62411ba94712838387587e4a99
jjb6000 <jonas.benkwitz@gmail.com>
2024-05-24 14:14:04 +0200

    firebase done

--- Übungen/firebase-übung/app.js
+++ Übungen/firebase-übung/app.js
@@ -1,19 +1,30 @@
-let outputContainer = document.getElementById('outputContainer');
+const outputContainer = document.getElementById('outputContainer');
 const FIREBASE = 'https://devacademytest-45922-default-rtdb.europe-west1.FIREBASEdatabase.app/';
 let user;
 
+document.getElementById('createForm').addEventListener('submit', event => {
+    event.preventDefault();
+    setUser(event);
+})
 
-function getEndpoint(e) {
-    let mail = e.srcElement[0].value;
-    let url = FIREBASE + mail.replaceAll('.', '--dot--').toLowerCase() + '/';
-    console.log(url);
-    getInfo(url)
+
+function getEndpoint(string) {
+    let endpoint = FIREBASE + string.replaceAll('.', '--dot--').toLowerCase() + '/';
+    return endpoint;
 }
 
 
-async function getInfo(endpoint) {
+async function getInfo(e) {
+    let endpoint = getEndpoint(e.srcElement[0].value);
     user = await getData(endpoint);
     outputContainer.innerHTML = userTemplateHTML(user.name, user.city, user.pet);
+}
+
+
+function setUser(e) {
+    let newEndpoint = getEndpoint(e.srcElement[1].value);
+    let user = new User(e.srcElement[0].value, e.srcElement[2].value, e.srcElement[3].value);
+    putData(newEndpoint, user);
 }
 
 
@@ -23,9 +34,29 @@ async function getData(url) {
 }
 
 
-async function postData(url) {
-    let data = await fetch(url + ".json").catch(errorFunction);
-    return await data.json();
+async function putData(url, data={}) {
+    let response = await fetch(url + ".json", {
+        method: 'PUT',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).catch(errorFunction);
+    return await response.json();
+}
+
+
+function deleteUser(e) {
+    let endpoint = getEndpoint(e.srcElement[0].value);
+    deleteData(endpoint)
+}
+
+
+async function deleteData(url) {
+    let response = await fetch(url + ".json", {
+        method: 'DELETE'
+    }).catch(errorFunction);
+    return await response.json();
 }
 
 
---
+++ Übungen/firebase-übung/class.js
@@ -0,0 +1,11 @@
+
+
+class User {
+    constructor(name, city, pet) {
+        this.name = name;
+        this.city = city;
+        this.pet = pet;
+    }
+}
+
+
