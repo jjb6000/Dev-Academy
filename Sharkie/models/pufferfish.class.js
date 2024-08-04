83bfdac322f5935abfe6ceb30429c1333bdaf07b
jjb6000 <jonas.benkwitz@gmail.com>
2024-08-04 22:06:53 +0200

    pufferfish swim not working

--- Sharkie/models/pufferfish.class.js
+++ Sharkie/models/pufferfish.class.js
@@ -1,12 +1,20 @@
 class Pufferfish extends MovableObject {
     height = 100;
     width = 100;
+    pathArray = [
+        '../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
+        '../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
+        '../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim3.png',
+        '../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
+        '../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim5.png'
+    ];
 
     constructor() {
         super().loadImage('../Sharkie/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.x = 680;
         this.y = Math.random() * 480;
         this.animate();
+        // this.movingAnimation(this.pathArray, 5, 150);
     }
 
     animate() {
