f959e97fac4e2028c7618449e880e0d592476ec6
jjb6000 <jonas.benkwitz@gmail.com>
2024-08-31 17:27:10 +0300

    changed name  imgage index const

--- Sharkie/models/status-bar.class.js
+++ Sharkie/models/status-bar.class.js
@@ -1,6 +1,6 @@
 class StatusBar extends DrawObject {
 STATUS_IMGs;
-statusImgObject = {
+statusToImgIndexTranslator = {
     100: 0,
     80: 1,
     60: 2,
@@ -18,7 +18,7 @@ statusImgObject = {
     reduceBar(health) {
         const possibleStatusArray = [100, 80, 60, 40, 20, 0];
         const barStatus = possibleStatusArray.find(status => status <= health);
-        this.img = this.STATUS_IMGs[this.statusImgObject[barStatus]];
+        this.img = this.STATUS_IMGs[this.statusToImgIndexTranslator[barStatus]];
     }
     
 }
