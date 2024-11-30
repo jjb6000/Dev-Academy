ea3cf943707de4fb3ff3bb190e75c7d5bcfdc309
jjb6000 <jonas.benkwitz@gmail.com>
2024-11-30 14:54:02 +0100

    fixed typo in path

--- Sharkie/menu/try-again-btn.class.js
+++ Sharkie/menu/try-again-btn.class.js
@@ -2,7 +2,7 @@ class TryAgainBtn extends DrawObject {
     
 
     constructor() {
-        super().loadImage('./img/menu/Try Again/Recurso 18.png');
+        super().loadImage('./img/menu/Try again/try-again.png');
         this.x = 220;
         this.y = 300;
         this.height = 80;
