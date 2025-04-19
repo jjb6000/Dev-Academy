02a2b63b1b9fb0b66dd3a22291b950d243df35d3
jjb6000 <jonas.benkwitz@gmail.com>
2025-04-19 20:54:28 +0200

    nav bar compo

--- Angular-first-steps/sakura/src/app/landingPage/landingPage.component.ts
+++ Angular-first-steps/sakura/src/app/landingPage/landingPage.component.ts
@@ -1,13 +1,20 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule],
-    templateUrl: './landingPage.component.html',
+    imports: [CommonModule, NavbarComponent],
+    template: `
+    <section class="hero-container">
+    <app-navbar />
+    <h1 class="font-raleway">SAKURA RAMEN</h1>
+    <span class="font-raleway" id="subtitle">THE BEST RAMEN IN TOWN</span>
+    </section>
+    `,
     styleUrls: ['./landingPage.component.scss']
 })
 
-export class LandingPage {}
+export class LandingPage { }
